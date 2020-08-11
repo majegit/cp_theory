9f1cff5b7c1629dc545c1cebdbdc0b2618b196c6
majegit <68793210+majegit@users.noreply.github.com>
2020-08-11 19:48:35 +0200

    vector_of_vectors done?

---
+++ array.cpp
@@ -0,0 +1,1 @@
+//How to make arrays in c++
---
+++ vector_of_vectors.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q, a, b;
+    //vector<vector<int>> vecs      THIS DOES NOT WORK!\
+    How many vectors is stored inside the outer vector must\
+    be known at compile time for this to work.
+    cin >> n >> q;
+    vector<vector<int>> vecs(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> a;
+        while(a--)
+        {
+            cin >> b;
+            cout << "b is: " << b << "\n";
+            vecs[i].push_back(b);
+        }
+    }
+    while(q--)
+    {
+        cin >> a >> b;
+        cout << vecs[a][b];
+    }
+    return 0;
+}
